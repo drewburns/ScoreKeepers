a667cda3c3af71355b401937b89944a0eb4f3d8d
drewburns <drewburnsbab@gmail.com>
2017-11-07 12:33:40 -0500

    added something not sure what

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+	before_action :authenticate_user!
+	def create
+		@comment = Comment.new(comment_params)
+		p "______________"
+		p @comment
+	    if @comment.save
+	    	redirect_back fallback_location: root_path, :notice => "Comment Created!"
+	    else
+	    	p @comment.errors
+	    	redirect_back fallback_location: root_path , :alert => "Please retry"
+	    end
+	end
+
+	def destroy
+	end
+
+	def vote
+			value = params[:type] == "up" ? 1 : -1
+			@comment = Comment.find(params[:id])
+			@user = User.find(params[:user_id])
+			if @user && @comment
+				@comment.add_or_update_evaluation(:votes, value, @user)
+		  	redirect_back fallback_location: root_path , :notice => "Vote counted"
+			else
+		 	 	redirect_back fallback_location: root_path, :alert => "Error"
+			end
+	end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:user_id, :post_id, :text )
+    end
+
+end

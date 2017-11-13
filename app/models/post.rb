412012e89a3abba21e05f86c77001f39fb792424
Timothy Noto <tnoto9@gmail.com>
2017-11-13 17:36:52 -0500

    not sure...

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+	mount_uploader :picture, PictureUploader
   validates :picture, presence: true
 
 

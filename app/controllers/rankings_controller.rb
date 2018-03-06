1922b7a9ce606de62fa73a8096157c3567e4a7dd
Max <t@t.com>
2018-03-06 09:48:47 -0500

    done

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,19 +1,23 @@
 class RankingsController < ApplicationController
 
   def NBAteam
-    @top_basketball = Team.where(sport_string: 'basketball').sort_by{|team| team.team_score}.reverse.first(30)
+    @teams = Team.all
+    @basketball = @teams.where(sport_string: "basketball")
   end
 
   def NBAcoach
-    @top_basketball = Team.where(sport_string: 'basketball').sort_by{|team| team.coach_score}.reverse.first(30)
+    @teams = Team.all
+    @basketball = @teams.where(sport_string: "basketball")
   end
 
   def NBAfrontoffice
-    @top_basketball = Team.where(sport_string: 'basketball').sort_by{|team| team.coach_score}.reverse.first(30)
+    @teams = Team.all
+    @basketball = @teams.where(sport_string: "basketball")
   end
 
   def NBAstadium
-    @top_basketball = Team.where(sport_string: 'basketball').sort_by{|team| team.stadium_score}.reverse.first(30)
+    @teams = Team.all
+    @basketball = @teams.where(sport_string: "basketball")
   end
 
 end
